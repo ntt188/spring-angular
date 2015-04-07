b7dd71a3c35e84c3962d79e7ce45b968d3f0768e
Tung NGUYEN <ntt188@gmail.com>
2015-04-07 23:46:08 +0200

    config

---
+++ spring/src/main/java/com/springangular/example/spring/RootConfig.java
@@ -0,0 +1,13 @@
+package com.springangular.example.spring;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan(basePackages = { "com.springangular.example.spring" }, 
+	excludeFilters = { @Filter(type = FilterType.ANNOTATION, value = EnableWebMvc.class) })
+public class RootConfig {
+}
