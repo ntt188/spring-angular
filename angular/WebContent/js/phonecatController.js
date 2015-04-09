68dada96ce713d438a6baf854540dd2ba0ee5261
ntt188 <ntt188@gmail.com>
2015-04-09 17:32:26 +0200

    Templating Links & Images

--- angular/WebContent/js/phonecatController.js
+++ angular/WebContent/js/phonecatController.js
@@ -2,7 +2,7 @@ var phonecatApp = angular.module('phonecatApp', []);
 
 phonecatApp.controller('PhoneListCtrl', function($scope, $http) {
 	$http.get('phones/phones.json').success(function(data) {
-		$scope.phones = data.splice(0, 5);
+		$scope.phones = data;
 	});
 
 	$scope.orderProp = 'age';
